2b12587a042e4ef93a32945438266cd127d3bd71
andrewhudson1977 <38694659+andrewhudson1977@users.noreply.github.com>
2022-08-04 11:21:02 +0100

    Initial Commit

---
+++ lib/main.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(colorSchemeSeed: Colors.blue),
+        home: MainPage(),
+      );
+}
+
+class MainPage extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  late WebViewController controller;
+
+  @override
+  Widget build(BuildContext context) => Scaffold(
+        appBar: AppBar(
+          title: const Text('Web View'),
+          actions: [
+            IconButton(
+              icon: const Icon(Icons.auto_fix_high),
+              onPressed: () async {
+                // 1. load new website url
+                controller.loadUrl('https://youtube.com');
+
+                // 2. get current website url
+                // final url = await controller.currentUrl();
+                // print('Current Website: $url');
+
+                // 3. go to previous website url
+                // if (await controller.canGoBack()) {
+                //   controller.goBack();
+                // }
+
+                // 4. reload website url
+                // controller.reload();
+              },
+            ),
+          ],
+        ),
+        body: WebView(
+          javascriptMode: JavascriptMode.unrestricted,
+          initialUrl: 'https://www.amazon.com/',
+          onWebViewCreated: (controller) {
+            this.controller = controller;
+          },
+          onPageStarted: (url) {
+            print('New Website: $url');
+          },
+        ),
+      );
+}
