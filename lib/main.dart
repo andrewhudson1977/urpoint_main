d37d0065b545821c971da2429256d17c64b3d532
andrewhudson1977 <38694659+andrewhudson1977@users.noreply.github.com>
2022-08-04 14:47:43 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -22,30 +22,6 @@ class _MainPageState extends State<MainPage> {
 
   @override
   Widget build(BuildContext context) => Scaffold(
-        appBar: AppBar(
-          title: const Text('Ur Point'),
-          actions: [
-            IconButton(
-              icon: const Icon(Icons.auto_fix_high),
-              onPressed: () async {
-                // 1. load new website url
-                controller.loadUrl('https://www.ur-cards.com');
-
-                // 2. get current website url
-                // final url = await controller.currentUrl();
-                // print('Current Website: $url');
-
-                // 3. go to previous website url
-                // if (await controller.canGoBack()) {
-                //   controller.goBack();
-                // }
-
-                // 4. reload website url
-                // controller.reload();
-              },
-            ),
-          ],
-        ),
         body: WebView(
           javascriptMode: JavascriptMode.unrestricted,
           initialUrl: 'https://www.ur-point.com/',
